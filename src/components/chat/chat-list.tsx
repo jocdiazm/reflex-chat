ca977a02bec80e08daf5899046ad0530fb11fcc1
Jose Carlos <jocdiazmuic@gmail.com>
2024-06-02 12:17:45 -0500

    styling autoscroll

--- src/components/chat/chat-list.tsx
+++ src/components/chat/chat-list.tsx
@@ -28,12 +28,12 @@ export const ChatMessageList = ({
 
   return (
     <div className="scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-200 flex max-h-full w-full max-w-full flex-1 flex-col  justify-start overflow-auto">
-      <div className="mb-[100px] flex flex-col-reverse  gap-6 px-2 py-5">
-        {messages
-          .map((message) => <ChatMessage message={message} key={message?.id} />)
-          .reverse()}
+      <div className="mb-[150px] flex flex-col  gap-6 px-2 py-5 " ref={ref}>
+        {messages.map((message) => (
+          <ChatMessage message={message} key={message?.id} />
+        ))}
       </div>
-
+      <div className="h-[200px] w-full" ref={ref}></div>
       <div
         className={cn(
           "flex items-center gap-2 p-2 text-sm opacity-50",
