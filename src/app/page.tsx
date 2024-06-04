fa2970dbb348183439f3a9c7f1ddeb39f2430aa5
Jose Carlos <jocdiazmuic@gmail.com>
2024-06-04 08:51:57 -0500

    adding home redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+import { EmptyScreen } from "@/components/chat/chat-empty";
 import { Button } from "@/components/ui/button";
+import { Bot } from "lucide-react";
 import Link from "next/link";
 
 export default async function Home() {
   return (
     <main className="min-w-screen max-w-screen grid max-h-dvh min-h-dvh place-content-center">
-      <Button className="w-fit">
-        <Link href="/chat">Go to chat</Link>
-      </Button>
+      <div className="flex flex-col items-center gap-4">
+        <EmptyScreen />
+        <Button className="w-fit" size="lg" asChild>
+          <Link href="/chat" className="flex items-center gap-2">
+            <Bot className="h-4 w-4" /> Let&apos;s chat!
+          </Link>
+        </Button>
+      </div>
     </main>
   );
 }
--- src/components/chat/chat-empty.tsx
+++ src/components/chat/chat-empty.tsx
@@ -4,22 +4,22 @@ export function EmptyScreen() {
   return (
     <div className="mx-auto mt-10 h-full max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
-        <h1 className="text-lg font-semibold">Hello from Reflex Chat!</h1>
+        <h1 className="text-center text-lg font-semibold">
+          Hello from Reflex Chat!
+        </h1>
         <p className="leading-normal text-muted-foreground">
-          This is a submission application for a chatbot interview at {""}
-          <ExternalLink href="https://nextjs.org">ReflexAI</ExternalLink>, using{" "}
-          <ExternalLink href="https://sdk.vercel.ai">Next.js</ExternalLink> app
-          directory,{" "}
-          <ExternalLink href="https://vercel.com/storage/kv">TRPC</ExternalLink>
+          This is a chatbot application made for {""}
+          <ExternalLink href="https://reflexai.com">ReflexAI</ExternalLink>,
+          using <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>{" "}
+          app directory,{" "}
+          <ExternalLink href="https://trpc.io/">TRPC</ExternalLink>
         </p>
         <p className="leading-normal text-muted-foreground">
           It uses{" "}
-          <ExternalLink href="https://vercel.com/blog/ai-sdk-3-generative-ui">
-            OpenAI API
-          </ExternalLink>{" "}
-          to simulate a conversation with a support agent from ReflexAI. You can
-          ask questions about the company, the products or the services they
-          offer.
+          <ExternalLink href="https://openai.com/">OpenAI API</ExternalLink> to
+          simulate a conversation with a support agent called ReflexChat. You
+          can ask questions about the company, the products or the services they
+          offer and in general, get brief answers to most questions.
         </p>
       </div>
     </div>
