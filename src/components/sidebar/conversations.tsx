05b5681704948511ff3b6b099ac29035f0969231
Jose Carlos <jocdiazmuic@gmail.com>
2024-06-04 23:39:00 -0500

    fixing button inside link

--- src/components/sidebar/conversations.tsx
+++ src/components/sidebar/conversations.tsx
@@ -34,10 +34,10 @@ export function ConversationsList({
   });
 
   const handleDelete = (id: string, isActive?: boolean) => {
-    deleteChat.mutate({ id });
     if (isActive) {
-      router.push("/chat");
+      router.push("/chat/");
     }
+    deleteChat.mutate({ id });
   };
 
   return (
@@ -61,27 +61,35 @@ export function ConversationsList({
               "focus-visible:ring:ring-primary group relative flex flex-col gap-0 rounded-lg px-1 text-muted-foreground transition-all ",
             )}
           >
-            <Link
-              href={`/chat/${conversation.id}`}
+            <div
               className={cn(
-                "flex flex-1 items-center justify-start gap-3 rounded-lg px-1 py-2 hover:bg-muted/60 hover:text-primary",
+                "hover:bg-muted/60 hover:text-primary",
                 active && "bg-muted text-primary/80 hover:bg-muted",
               )}
             >
-              <MessageSquare className="h-4 w-4 shrink-0 text-inherit" />
-              <span className="max-w-[150px] truncate  lg:max-w-[200px]">
-                {capitalize(conversation.description)}
-              </span>
-
+              <Link
+                href={`/chat/${conversation.id}`}
+                className={cn(
+                  "flex flex-1 items-center justify-start gap-3 rounded-lg px-1 py-2 ",
+                )}
+              >
+                <MessageSquare className="h-4 w-4 shrink-0 text-inherit" />
+                <span className="max-w-[150px] truncate  lg:max-w-[200px]">
+                  {capitalize(conversation.description)}
+                </span>
+              </Link>
               <Button
                 variant="icon"
                 size="icon"
-                className="absolute right-2 hidden justify-self-end text-muted-foreground hover:flex group-hover:flex"
-                onClick={() => handleDelete(conversation.id, active)}
+                className="absolute right-2 top-2  hidden justify-self-end text-muted-foreground hover:flex group-hover:flex"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDelete(conversation.id, active);
+                }}
               >
                 <Trash2 className=" text-inherit hover:text-primary" />
               </Button>
-            </Link>
+            </div>
             <TimeAgo
               timestamp={conversation.createdAt}
               className={cn(
